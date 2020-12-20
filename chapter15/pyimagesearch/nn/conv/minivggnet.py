06559633dbf62f84fb4bf9206f40ea2d966ef550
reynaldiaditia <reynaldiaditia220@gmail.com>
2020-12-20 18:49:41 +0700

    without BN

--- chapter15/pyimagesearch/nn/conv/minivggnet.py
+++ chapter15/pyimagesearch/nn/conv/minivggnet.py
@@ -28,10 +28,10 @@ class MiniVGGNet:
         model.add(Conv2D(32, (3, 3), padding="same", 
             input_shape=inputShape))
         model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
+        # model.add(BatchNormalization(axis=chanDim))
         model.add(Conv2D(32, (3, 3), padding="same"))
         model.add(Activation("relu"))
-        model.add(BatchNormalization(axis=chanDim))
+        # model.add(BatchNormalization(axis=chanDim))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
